c480feb5d3096b241b9fb6c4ad381c0bc8e93a9e
Ryan Stockdale <ryan.stockdale@vaya.net.au>
2018-03-16 08:11:06 +1100

    Initial Commit
    Added Serverless/Lambda support
    Example timeline JSON files
    Extra images
    JQuery Modal for file view
    Makefile

--- README.md
+++ README.md
@@ -1,12 +1,81 @@
-Vertical Timeline
-=========
+# Serverless Vertical Timeline 
 
-An easy to customize, responsive timeline.
+An easy to customize, responsive timeline in AWS Lambda. 
 
-[Article on CodyHouse](http://codyhouse.co/gem/vertical-timeline/)
+Use JSON files for multiple timelines.
 
-[Demo](http://codyhouse.co/demo/vertical-timeline/index.html)
- 
-[Terms](http://codyhouse.co/terms/)
+[Original article on CodyHouse](http://codyhouse.co/gem/vertical-timeline/)
 
-Icons: [Nucleo](https://nucleoapp.com)
+## Prerequisites ##
+
+- Docker
+- Docker Compose
+- Make
+- AWS Admin Access
+
+## Environment variables
+
+Make sure you have set your environment variables properly or create a file `.env`. The file `.env.template` contains the environment variables that are used by the application. Keys found in LastPass.
+
+## Make Usage ##
+
+```bash
+# using . env.template for .env as an example
+$ make dotenv DOTENV=.env.template
+# OR 
+$ make .env
+# Deploy the lambda stack, make sure to comment out serverless offline in the serverless.yml file
+$ make deploy
+# Remove the lambda stack
+$ make remove
+```
+
+## Develop ##
+
+All docs moved to FreshDesk and direct linked. Images hosted in the LiveConnected My Account.
+
+Uncomment the Offline plugin in serverless.yml 
+
+```bash
+# using offline mode
+$ make offline
+```
+
+## Multiple timelines
+
+Add JSON files in the src/projects/ directory. 
+
+### Structure
+
+```
+[
+	{
+		"title" : "Initial Spike with Deloitte Digital", 
+		"icon" : "flash-21", 
+		"desc" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.", 
+		"files" : [{
+			"file": "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+		}],
+		"date" : "Aug 18"
+	},
+	{
+		"title" : "Proof of concept", 
+		"icon" : "settings-gear-63", 
+		"desc" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.", 
+		"files" : [{
+			"file": "https://www.w3schools.com/w3css/img_lights.jpg"
+		}],
+		"date" : "Sept 05"
+	}
+]
+```
+
+
+
+## Credits
+
+Virtical Timeline https://github.com/CodyHouse/vertical-timeline
+
+Static File Handler https://www.npmjs.com/package/serverless-aws-static-file-handler
+
+Icons https://nucleoapp.com/
---
+++ src/js/main_.js
@@ -0,0 +1,49 @@
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
+
+//This is the plugin to create the modal
+
+(function(a){a.createModal=function(b){defaults={title:"",message:"Your Message Goes Here!",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 100%;overflow-y: auto;"':"";html='<div class="modal fade modal-lg" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true" style="display:none;">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal"  style="display:none;">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
+
+//This function will pop the modal with the image or PDF embeded in either an iFrame or img tag	
+	
+$(function(){    
+    $('.view-in-modal').on('click',function(){
+        var doc_link = $(this).attr('href'),
+			doc_frame = doc_link.includes(".pdf") ? '<div class="iframe-container"><iframe src="'+doc_link+'"></iframe></div>' : '<img src="'+doc_link+'" />';
+		
+		
+        $.createModal({
+        title:'Document Viewer',
+        message: doc_frame,
+        closeButton:true,
+        scrollable:false
+        });
+        return false;        
+    });    
+})
+	
